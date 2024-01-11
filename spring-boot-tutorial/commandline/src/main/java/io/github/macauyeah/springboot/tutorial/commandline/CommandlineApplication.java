8b153ce7aae3e9983ca202214d957ad91bf58feb
macauyeah <macauyeahwebmail@gmail.com>
2024-01-11 11:04:12 +0800

    init from initializer; then delete wrapper

---
+++ spring-boot-tutorial/commandline/src/main/java/io/github/macauyeah/springboot/tutorial/commandline/CommandlineApplication.java
@@ -0,0 +1,13 @@
+package io.github.macauyeah.springboot.tutorial.commandline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommandlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommandlineApplication.class, args);
+	}
+
+}
---
+++ spring-boot-tutorial/commandline/src/test/java/io/github/macauyeah/springboot/tutorial/commandline/CommandlineApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.macauyeah.springboot.tutorial.commandline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommandlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
