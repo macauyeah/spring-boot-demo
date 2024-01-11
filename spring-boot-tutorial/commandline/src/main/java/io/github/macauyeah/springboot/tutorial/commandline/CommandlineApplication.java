ebaf7ddb958c5ed7ddc6a0864f370c8f0ddc288a
macauyeah <macauyeahwebmail@gmail.com>
2024-01-11 14:33:22 +0800

    get argument

--- spring-boot-tutorial/commandline/src/main/java/io/github/macauyeah/springboot/tutorial/commandline/CommandlineApplication.java
+++ spring-boot-tutorial/commandline/src/main/java/io/github/macauyeah/springboot/tutorial/commandline/CommandlineApplication.java
@@ -1,13 +1,35 @@
 package io.github.macauyeah.springboot.tutorial.commandline;
 
+import java.util.Set;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CommandlineApplication {
+public class CommandlineApplication implements ApplicationRunner {
+	static Logger LOG = LoggerFactory.getLogger(CommandlineApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(CommandlineApplication.class, args);
+	}
+
+	// mvn clean compile spring-boot:run -Dspring-boot.run.arguments="--JarCommandOptions=test --JarCommandOptions=test2 --SomeCommandOptions=some --SomeCommandOptions=some2"
+	// java -jar target/commandline-0.0.1-SNAPSHOT.jar --JarCommandOptions=test --JarCommandOptions=test2 --SomeCommandOptions=some --SomeCommandOptions=some2
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		Set<String> options = args.getOptionNames();
+		options.stream().forEach(option -> {
+			LOG.debug("option name:" + option);
+
+			args.getOptionValues(option).forEach(optionValue -> {
+				LOG.debug("option values:" + optionValue);
+			});
+		});
+		LOG.debug("program end.");
 	}
 
 }
--- spring-boot-tutorial/commandline/src/test/java/io/github/macauyeah/springboot/tutorial/commandline/CommandlineApplicationTests.java
+++ spring-boot-tutorial/commandline/src/test/java/io/github/macauyeah/springboot/tutorial/commandline/CommandlineApplicationTests.java
@@ -3,7 +3,7 @@ package io.github.macauyeah.springboot.tutorial.commandline;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(args = "--app.test=one")
 class CommandlineApplicationTests {
 
 	@Test
