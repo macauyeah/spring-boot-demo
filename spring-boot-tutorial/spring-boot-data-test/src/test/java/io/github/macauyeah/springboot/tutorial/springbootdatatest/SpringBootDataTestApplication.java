735f312343b5fce82d5940a3ae154e222e3ec04d
macauyeah <macauyeahwebmail@gmail.com>
2024-03-08 16:52:32 +0800

    add test module for spring data

---
+++ spring-boot-tutorial/spring-boot-data-test/src/test/java/io/github/macauyeah/springboot/tutorial/springbootdatatest/SpringBootDataTestApplication.java
@@ -0,0 +1,13 @@
+package io.github.macauyeah.springboot.tutorial.springbootdatatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDataTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDataTestApplication.class, args);
+	}
+
+}
---
+++ spring-boot-tutorial/spring-boot-data-test/src/test/java/io/github/macauyeah/springboot/tutorial/springbootdatatest/SpringBootDataTestApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.macauyeah.springboot.tutorial.springbootdatatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDataTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
