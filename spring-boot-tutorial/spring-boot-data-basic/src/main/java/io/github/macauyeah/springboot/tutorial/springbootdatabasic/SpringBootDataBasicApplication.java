a8dbce6ce5cff26669cfb01d4f869148c5d8618b
macauyeah <macauyeahwebmail@gmail.com>
2024-04-11 19:43:42 +0800

    revise basic runner example so that it can skip runner by controlling profile value;

--- spring-boot-tutorial/spring-boot-data-basic/src/main/java/io/github/macauyeah/springboot/tutorial/springbootdatabasic/SpringBootDataBasicApplication.java
+++ spring-boot-tutorial/spring-boot-data-basic/src/main/java/io/github/macauyeah/springboot/tutorial/springbootdatabasic/SpringBootDataBasicApplication.java
@@ -1,41 +1,12 @@
 package io.github.macauyeah.springboot.tutorial.springbootdatabasic;
 
-import java.util.UUID;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
-public class SpringBootDataBasicApplication implements ApplicationRunner {
-	@Autowired
-	private AppleRepo appleRepo;
-	@Autowired
-	private JdbcTemplate jdbcTemplate;
+public class SpringBootDataBasicApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDataBasicApplication.class, args);
 	}
-	@Override
-	public void run(ApplicationArguments args) throws Exception {
-		Apple apple = new Apple();
-		apple.setUuid(UUID.randomUUID().toString());
-		apple.setWeight(100.0);
-		apple.setGravity(1000.0);
-		appleRepo.save(apple);
 
-		Apple apple2 = new Apple();
-		apple2.setUuid(UUID.randomUUID().toString());
-		apple2.setGravity(1000.0);
-		appleRepo.save(apple2);
-
-		dump();
-	}
-
-
-	private void dump(){
-		jdbcTemplate.execute("SCRIPT TO 'dump.sql'");
-	}
 }
--- spring-boot-tutorial/spring-boot-data-basic/src/test/java/io/github/macauyeah/springboot/tutorial/springbootdatabasic/SpringBootDataBasicSkipRunnerTests.java
+++ spring-boot-tutorial/spring-boot-data-basic/src/test/java/io/github/macauyeah/springboot/tutorial/springbootdatabasic/SpringBootDataBasicSkipRunnerTests.java
@@ -5,15 +5,17 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-class SpringBootDataBasicApplicationTests {
+@ActiveProfiles("skip")
+class SpringBootDataBasicSkipRunnerTests {
 	@Autowired
 	AppleRepo appleRepo;
 
 	@Test
 	void contextLoads() {
-		assertTrue(appleRepo.count() > 0);
+		assertTrue(appleRepo.count() == 0);
 	}
 
 }
